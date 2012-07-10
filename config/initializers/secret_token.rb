9925bbce19c89b28dd383279e3580f2149edba6c
Anush Jayaraman <anushjay@gmail.com>
2012-07-09 19:56:08 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandumbdudeV1::Application.config.secret_token = '869062e6ecee3f5fbbbbf5180aae844d8feeff17a9677611dd1cc6b140f372102a541af03a29c1b91782b9c65dff9794ad896621898d5da73e00e02acfbc62e5'
